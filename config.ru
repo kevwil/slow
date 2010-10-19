25007e005630d5523b8abb616c9c5f0b0cbb3f17
Kevin Williams <kevwil@gmail.com>
2010-10-19 13:43:09 -0600

    initial commit and push out to github

---
+++ config.ru
@@ -0,0 +1,21 @@
+##########################
+# This Rack configuration script will allow the 'slow' app
+# to run under any Rack-supporting Ruby app server. This enables
+# you to run the 'slow' app in the background (if the app server allows it).
+#
+# My recommendation is Thin.
+# * gem install thin
+#
+# To run this app in the background, install Thin and execute this way to run it:
+# * thin -d (or --daemonize) -R (or --rackup) config.ru -p (or --port) 80 start
+# * the default port for Thin is 3000, so if you don't specify a port then it would use 3000.
+#
+# You can run the same command from the same directory but use "stop" instead of "start" to
+# kill the background process. Thin attempts to keep track of the PID of the
+# background process it spawned.
+
+ENV["RACK_ENV"] = 'production'
+
+require 'slow'
+
+run Sinatra::Application
---
+++ slow.rb
@@ -0,0 +1,74 @@
+##########################
+# This Ruby script is a Sinatra app (http://sinatrarb.com).
+# To run it, you need:
+# * ruby (1.8.7 is best, should work in 1.9.2, might work in jruby, etc.)
+# * rubygems
+# * sinatra gem
+# * rack-contrib gem
+#
+# call this file like this: (you need to be root to listen on a port < 1024)
+# (sudo) ruby -rubygems slow.rb -p 80 -e production
+#
+# This will listen like a server but will run in the foreground.
+# You need to use the config.ru file to run in the background. Read that file for details.
+#
+# or, to skip the port, environment, and rubygems
+# parts, you can uncomment the rubygems and
+# config variables lines below. Uncommenting the
+# Rack::Lint part will just slow the server down.
+#
+#
+# To call the service, any request path (URI after server and port) will work.
+# * curl -i http://localhost/foo/bar/baz
+# * curl -i http://localhost/Affinity/v1/session/create
+#
+# Other HTTP verbs also work, specifically GET, POST, PUT, DELETE, and HEAD.
+# * curl -i -XHEAD http://localhost/foo/bar
+# * curl -i -XPUT http://localhost/foo/bar
+# * curl -i -XDELETE http://localhost/foo/bar
+# * curl -i -XPOST -d "post_param1=hello&post_param2=world" http://localhost/foo/bar
+#
+# The script supports POST method override, too.
+# * curl -i -XPOST -d "_method=PUT&param1=update_me" http://localhost/my/personal/info
+#
+# There are two optional querystring parameters you can provide to tweak the behavior.
+# * To return a specific error code, with debug info in the body:
+# ** curl -i http://localhost/foo/bar/baz?errorcode=403
+# * To fail after the timeout is complete, rather than returning a successful 200 OK status.
+# ** curl -i http://localhost/foo?fail=true
+##########################
+
+### gems required to make this file run as a web server
+# require 'rubygems'
+require 'sinatra'
+require 'rack/contrib'
+
+### extra Rack middleware to help this run correctly
+# use Rack::Lint
+use Rack::BounceFavicon
+# use Rack::Lint
+
+### it's tempting to add this Middleware to enable throttling
+# http://datagraph.rubyforge.org/rack-throttle/
+
+### config variables when run in foreground
+# set :environment, :production
+# set :port, 80
+set :method_override, true
+
+### configure the delay, in milliseconds
+timeout = 5000
+
+### the app itself :
+# all five HTTP methods do the same thing
+%w{get post put delete head}.each do |method|
+	# call the method for each HTTP method, with a wildcard path
+	send method.to_sym, '/*' do
+		# define the block of behavior for each call
+		# block the request (sleep the thread) for the given delay time, in seconds
+		sleep(timeout/1000)
+		# return error code with message, if :errorcode parameter is given
+		halt params[:errorcode].to_i, "#{request.request_method} request to #{request.path_info} failed after #{timeout} milliseconds with error code #{params[:errorcode]}\n" if params[:errorcode]
+		halt 500 if params[:fail]
+	end
+end
