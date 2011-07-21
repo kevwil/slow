71589eaa94cf95cd99a50242c809b83b724ea39f
Kevin Williams <kevwil@gmail.com>
2011-07-21 12:47:00 -0600

    add proxying of call to other host

--- slow.rb
+++ slow.rb
@@ -33,7 +33,9 @@
 #
 # The script supports a proxy behavior, where it will call the same URI on a remote host
 # after the timeout has expired, and return the result.
-# * curl -i "http://localhost/foo?proxyTo=http%3A%2F%2Flocalhost2%2F"
+# * curl -i "http://localhost/foo?proxyTo=http%3A%2F%2Flocalhost%3A8080"
+#
+# A default proxy can be set using the _proxy_ value below
 #
 # There are three optional querystring parameters you can provide to tweak the behavior.
 # * To return a specific error code, with debug info in the body:
@@ -48,7 +50,8 @@
 # require 'rubygems'
 require 'sinatra'
 require 'rack/contrib'
-require 'open-uri'
+require 'net/http'
+require 'uri'
 require 'cgi'
 
 ### extra Rack middleware to help this run correctly
@@ -65,6 +68,11 @@ use Rack::BounceFavicon
 set :method_override, true
  
 ### the app itself :
+
+# set this value to use a default target host when proxying
+# (set to protocol://host or protocol://host:port)
+# proxy = 'http://localhost:3000'
+
 # all five HTTP methods do the same thing
 %w{get post put delete head}.each do |method|
   # call the method for each HTTP method, with a wildcard path
@@ -79,8 +87,27 @@ set :method_override, true
     # block the request (sleep the thread) for the given delay time, in seconds
     sleep(timeout/1000)
     # run proxy call and return result, if desired
-    if params[:proxyTo]
-      
+    if proxy or params[:proxyTo]
+      if proxy
+        uri = URI.parse(proxy)
+        out_path = request.fullpath
+      else
+        uri = URI.parse(Rack::Utils.unescape(params[:proxyTo]))
+        qsHash = CGI::parse(request.query_string)
+        qsHash.delete 'proxyTo'
+        qs = qsHash.map{|k,v| "#{CGI.escape(k)}=#{CGI.escape(v)}"}.join("&")
+        out_path = "#{request.path}?#{qs}"
+      end
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        if request.post? or request.put?
+          puts request.POST.inspect
+          returned = http.send method.to_sym, out_path, request.POST.to_s
+        else
+          returned = http.send method.to_sym, out_path
+        end
+        status(returned.code)
+        return returned.body
+      end
     end
     # return error code with message, if :errorcode parameter is given
     halt params[:errorcode].to_i, "#{request.request_method} request to #{request.path_info} failed after #{timeout} milliseconds with error code #{params[:errorcode]}\n" if params[:errorcode]
